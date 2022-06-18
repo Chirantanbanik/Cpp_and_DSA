b75b9c1026372be8376399a24c50950d92e02780
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-18 20:12:34 +0530

    Update Delete.cpp

--- Array/Delete.cpp
+++ Array/Delete.cpp
@@ -3,34 +3,32 @@
 #include<iostream>
 using namespace std;
 
-int insert(int arr[], int n, int x, int cap, int pos){
-    if(n==cap){
-        return n;
+int deleteEle(int arr[], int n, int x){
+    int i = 0;
+    for( i=0; i<n; i++){
+        if(arr[i] == x)
+            break;
+        }
+        if(i==n)
+            return n;
+        
+        for(int j=i; j<n-1; j++){
+            arr[j] = arr[j+1];
+        }
+          return(n-1);
     }
 
-    int idx = pos - 1;
 
-    for(int i=n-1; i>=idx; i--){
-        arr[i+1] = arr[i];
-    }
-
-    arr[idx] = x;
-    return(n+1);
-
-}
 
 int main(){
-    int arr[6], cap=6, n=5;
-    arr[0]=1, arr[1]=2, arr[2]=3, arr[3]=4, arr[4]=5, arr[5]=6;
-    cout<<"Before Insertion"<<endl;
-    for(int i=0; i<n; i++){
+    int arr[] = {3,1,9,0,6,7,10}, n = 7, x = 1;
+    cout<<"Before Deletion"<<endl;
+    for(int i=0; i<n; i++)
         cout<<arr[i]<<endl;
-    }
 
-int x=0, pos=0;
-n = insert(arr, n, x, cap, pos);
-cout<<"After Insertion"<<endl;
-    for(int i=0; i<n; i++){
+    n = deleteEle(arr, n, x);
+    cout<<"After deletion"<<endl;
+    for(int i=0; i<n; i++)
         cout<<arr[i]<<endl;
     }
-}
+    
