84feb044a9dadd525e0c2402b7a3653e21b310e4
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-23 21:52:35 +0530

    Update Binary Search.cpp

--- Searching/Binary Search.cpp
+++ Searching/Binary Search.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int binarySearch(int arr[], int n, int key){
-    int low = 0, high = n-1;
-    int mid = (low+high)/2;
-        if(arr[mid]==key){
-            return key;
-        }  else if(arr[mid]<key) {
+    int low = 0, high = n - 1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]==key)
+            return mid;
+        else if(arr[mid]>key) 
+            high = mid - 1;
+        else 
             low = mid + 1;
-        }  else 
-           high = mid - 1;
+        }
+
+        return -1;
     }
 
-    int main(){
-        int arr[] = {4,1,9,3,10}, n=5, key=1;
-        cout<<"The index of the element is ";
-        cout<<binarySearch(arr,n, key)<<endl;
+int main(){
+    int arr[] = {2,4,6,8,10}, n=5, key=10;
+    cout<<"The index of the element is "<<endl;
+    cout<<binarySearch(arr, n, key);
 }
+
