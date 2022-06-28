684767aef67d8711656e8d15d31f033f5c2433b5
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-28 20:45:04 +0530

    Create Find count of a Number.cpp

---
+++ Mathematics/Find count of a Number.cpp
@@ -0,0 +1,18 @@
+// Find the count of a number
+
+include<iostream>  
+using namespace std;
+
+int getCount(int n){
+    int count = 0;
+    while(n!=0){
+        n=n/10;
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    int n = 12345;
+    cout<<"The count of the number is: "<<getCount(n)<<endl;
+}
