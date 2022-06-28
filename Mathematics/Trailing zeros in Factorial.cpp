395ec76ea809fc5a08e92336cb39830a0cdfd2e7
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-28 22:38:39 +0530

    Update Trailing zeros in Factorial.cpp

--- Mathematics/Trailing zeros in Factorial.cpp
+++ Mathematics/Trailing zeros in Factorial.cpp
@@ -1,2 +1,21 @@
 // Find the Trailing of a Factorial.
 
+// Logic: For Trailing zeros we will take res = 0, use a for loop which starts from 5 and increments by i=i*5, find res=res+n/5 and at last return res.
+// In the main function we put the input to find the value
+
+
+#include<iostream>
+using namespace std;
+
+int trailingZeros(int n){
+    int res = 0;
+    for(int i=5; i<=n; i=i*5){
+        res=res+n/i;
+        return res;
+    }
+}
+
+int main(){
+    int n = 7;
+    cout<<trailingZeros(n)<<endl;
+}
