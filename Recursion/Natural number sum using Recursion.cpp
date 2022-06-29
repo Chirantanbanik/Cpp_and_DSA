0cfda26ac8bc0df835f70fd830ac19ac8829a8c5
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-29 20:23:19 +0530

    Create Natural number sum using Recursion.cpp

---
+++ Recursion/Natural number sum using Recursion.cpp
@@ -0,0 +1,17 @@
+// Natural Number sum using Recursion.
+
+#include<iostream>
+using namespace std;
+
+int getSum(int n){
+    if(n==0)
+    return n;
+
+    return n + getSum(n-1);
+    
+}
+
+int main(){
+    cout<<getSum(3);
+}
+
