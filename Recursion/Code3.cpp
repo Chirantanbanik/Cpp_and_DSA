98a66fb66528755241a84b68c04fd63172c05b00
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-29 17:13:38 +0530

    Update Code3.cpp

--- Recursion/Code3.cpp
+++ Recursion/Code3.cpp
@@ -22,8 +22,8 @@ int main(){
 
 // fun(7) -> fun(3) -> fun(1) -> fun(0)
 
-` for any odd number when you do a remainder(%) with 2 you get
- output as 1 and for even you will get output as 0. `
+// for any odd number when you do a remainder(%) with 2 you get
+// output as 1 and for even you will get output as 0. 
  
  // fun(7), fun(3) & fun(1) are odd so output = 111
 
