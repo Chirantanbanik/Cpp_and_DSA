2ab44a827049648fd2c56f8528b6728e65c8e1e6
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-29 19:26:35 +0530

    Update Factorial.cpp

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,1 +1,18 @@
+// Find factorial using Recursion
 
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0 || n==1)
+        return 1;
+
+        return n*fact(n-1);
+        
+}
+
+int main(){
+    cout<<fact(3);
+}
+
+// Output: 6
