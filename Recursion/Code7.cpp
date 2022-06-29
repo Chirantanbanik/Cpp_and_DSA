4e9eaf820b9ffe07d754d3abc0ea1723ae05ea74
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-29 17:39:27 +0530

    Create Code7.cpp

---
+++ Recursion/Code7.cpp
@@ -0,0 +1,26 @@
+// Print all the numbers from N to 1 using Recursion
+
+#include<iostream>
+using namespace std;
+
+void printNto1(int n){
+    if(n==0)
+        return;
+
+        cout<<n<<endl;
+        printNto1(n-1);
+}
+
+int main(){
+    printNto1(4);
+}
+
+// Output:
+// 4
+// 3
+// 2
+// 1
+
+// Logic:
+// printNto1(4) -> printNto1(3) -> printNto1(2) -> printNto1(1) -> printNto1(0)
+//      4               3               2               1 
