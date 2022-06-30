d4a0062b297413af4c81f557bd73708ab8578dbc
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-30 13:03:21 +0530

    Create Sum of Digits.cpp

---
+++ Recursion/Sum of Digits.cpp
@@ -0,0 +1,20 @@
+// Find the sum of Digits by using Recursion
+
+#include<iostream>
+using namespace std;
+
+int sumDigit(int n){
+    if(n==0)
+    return 0;
+
+    return sumDigit(n/10) + (n%10);
+}
+
+int main(){
+    cout<<sumDigit(55);
+}
+
+
+// Logic: 
+// n%10 -> gives the last digit
+// n/10 -> removes the last digit
