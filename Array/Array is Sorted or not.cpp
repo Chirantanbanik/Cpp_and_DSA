7a8db56bac7fcfc6102b2952a8e8f88be9e5862d
Chirantan Banik <banikchirantan16@gmail.com>
2022-07-02 23:31:11 +0530

    Update Array is Sorted or not.cpp

--- Array/Array is Sorted or not.cpp
+++ Array/Array is Sorted or not.cpp
@@ -2,7 +2,7 @@
 
 // Algorithm:
 // 1. make an function isSorted and put arguments arr[], n.
-// 2. run a for loop from 0 to n and check if arr[i]<arr[i-1] which is not possible so return false
+// 2. run a for loop from 1 to n and check if arr[i]<arr[i-1] which is not possible so return false
 // otherwise return true.
 // 3. In the main function initialize the arr[] with the values and give the size i.e. n and cout
 // the function isSorted(arr, n).
@@ -11,7 +11,7 @@
 using namespace std;
 
 bool isSorted(int arr[], int n){
-	for(int i=0; i<n; i++){
+	for(int i=1; i<n; i++){
 		if(arr[i]<arr[i-1]){
 			return false;
 		}
@@ -20,6 +20,6 @@ bool isSorted(int arr[], int n){
 }
 
 int main(){
-	int arr[] = {1,2,3,5,4}, n=3;
+	int arr[] = {1,2,3,5,4}, n=5;
 	cout<<isSorted(arr,n)<<endl;
 }
