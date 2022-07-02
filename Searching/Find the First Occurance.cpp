64bfbb5e79957bdd4a357a3e27d476040bfa2db8
Chirantan Banik <banikchirantan16@gmail.com>
2022-07-02 23:03:33 +0530

    Create Find the First Occurance.cpp

---
+++ Searching/Find the First Occurance.cpp
@@ -0,0 +1,15 @@
+// Logic: Just traverse throught the array as soon as you find the element return the index.
+
+#include<iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int x){
+    for(int i=0; i<n; i++)
+    if(arr[i]==x)
+    return i;
+}
+
+int main(){
+    int arr[] = {1,2,2,3,4,4,5,5}, n = 8, x = 5;
+    cout<<firstOcc(arr, n, x);
+}
