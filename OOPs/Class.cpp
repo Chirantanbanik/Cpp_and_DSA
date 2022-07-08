b6d245b04f7c5a452172a0f15a260b5684be8571
Chirantan Banik <banikchirantan16@gmail.com>
2022-07-08 23:01:37 +0530

    Create Class.cpp

---
+++ OOPs/Class.cpp
@@ -0,0 +1,22 @@
+// Write a Class for a Rectangle
+
+#include<iostream>
+using namespace std;
+
+class Rectangle{
+    public:
+    int length;
+    int breadth;
+
+    int area(){
+        return length*breadth;
+    }
+    
+};
+
+int main(){
+    Rectangle r;
+    r.length = 10;
+    r.breadth = 20;
+    cout<<r.area()<<endl;
+}
